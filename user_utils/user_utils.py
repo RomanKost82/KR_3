5ec42f7d205fdadc391669148dbf330c0df52b5c
RomanKost82 <r79006482751@gmail.com>
2024-04-07 22:08:06 +0300

    add def for sorted by state == EXECUTED

--- user_utils/user_utils.py
+++ user_utils/user_utils.py
@@ -1,7 +1,36 @@
 import json
-
+import os
 
 def load_data():
-    pass
+    # Получаем текущую директорию скрипта
+    current_directory = os.path.dirname(__file__)
+    # Переходим на один уровень вверх (родительская директория)
+    parent_directory = os.path.dirname(current_directory)
+    # Формируем путь к файлу operations.json, используя относительный путь к папке data
+    json_file_path = os.path.join(parent_directory, 'data', 'operations.json')
 
-def
+    with open(json_file_path, 'r', encoding='UTF-8') as json_file:
+        data_operations = json.load(json_file)
+
+
+    return data_operations
+
+
+data = load_data()
+
+def executed_operation(data):
+    executed_list = {}
+    for i in data:
+        if 'state' in i and i['state'] == 'EXECUTED':
+            executed_list.update(i)
+
+
+    print(executed_list)
+
+executed = executed_operation(data)
+
+# def sorted_by_date():
+
+
+# if __name__ == '__main__':
+#     print(load_data())
