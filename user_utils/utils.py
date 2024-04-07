ee8da02d1d66b1e81e47c40b7d940a6e9193e1c9
RomanKost82 <r79006482751@gmail.com>
2024-04-08 02:44:39 +0300

    add def print_operation, PEP8

--- user_utils/utils.py
+++ user_utils/utils.py
@@ -2,11 +2,18 @@ import json
 import os
 from datetime import datetime
 
+
 def load_data():
+    """
+    Загружаем json файл
+    :return: dict словарь данных по операциям
+    """
     # Получаем текущую директорию скрипта
     current_directory = os.path.dirname(__file__)
+
     # Переходим на один уровень вверх (родительская директория)
     parent_directory = os.path.dirname(current_directory)
+
     # Формируем путь к файлу operations.json, используя относительный путь к папке data
     json_file_path = os.path.join(parent_directory, 'data', 'operations.json')
 
@@ -19,9 +26,14 @@ def load_data():
 data = load_data()
 
 
-def executed_operation(data):
+def executed_operation(data_dict):
+    """
+    Формируем список словарей выполненных операций
+    :param data_dict: dict
+    :return: list
+    """
     executed_list = []
-    for item in data:
+    for item in data_dict:
         if 'state' in item and item['state'] == 'EXECUTED':
             executed_list.append(item)
 
@@ -30,12 +42,14 @@ def executed_operation(data):
 
 executed_state_list = executed_operation(data)
 
-print(executed_state_list)
-
-print(executed_state_list[1]['state'])
-
 
 def sorted_by_date(executed_list):
+    """
+    Сортируем операции по дате и времени исполнения.
+    Возвращаем последние 5 операций
+    :param executed_list: list
+    :return: dict
+    """
     for i in executed_list:
         i['date'] = ' '.join(i['date'][:-7].split('T'))
 
@@ -46,16 +60,17 @@ def sorted_by_date(executed_list):
 
 sorted_dict_list = sorted_by_date(executed_state_list)
 
-print(sorted_dict_list)
 
-print(len(sorted_dict_list))
-
-def prepare_info_for_print(sorted_dict_list):
-    for i in sorted_dict_list:
+def prepare_info_for_print(output_data):
+    """
+    Форматируем данные словаря в требуемый вид для вывода
+    :param output_data: dict
+    :return: dict
+    """
+    for i in output_data:
         format_date = datetime.strptime(i['date'], '%Y-%m-%d %H:%M:%S')
         new_format_date = format_date.strftime('%d.%m.%Y %H:%M:%S')
         i['date'] = new_format_date
-        print(new_format_date)
 
         if 'from' in i:
             account_from = i['from'].split()
@@ -63,21 +78,35 @@ def prepare_info_for_print(sorted_dict_list):
             modify_account = i['from'][len(i['from']) - 1]
             modify_account = (modify_account[:6] + '*' * 6 + modify_account[len(modify_account)-4:])
             modify_account = ' '.join(modify_account[i:i + 4] for i in range(0, len(modify_account), 4))
-            print(modify_account)
+            i['from'][len(i['from']) - 1] = modify_account
         else:
-            i['from'] = 'Данные отпровителя отсутствуют'
+            i['from'] = '-'
 
         if 'to' in i:
             account_to = i['to'].split()
             i['to'] = account_to
             i['to'][1] = '**' + i['to'][1][len(i['to'][1])-4:]
-            print(account_to)
         else:
-            i['to'] = 'Данные получателя отсутствуют'
+            i['to'] = '-'
+
+    return output_data
 
 
-    print(sorted_dict_list)
-
-prepare_info_for_print(sorted_dict_list)
+modify_operation_dict = prepare_info_for_print(sorted_dict_list)
 
 
+def print_operation(printed_operation):
+    """
+    Вывод последних 5-и операций
+    :param printed_operation: dict
+    :return: None
+    """
+    for i in printed_operation:
+        print(f'{i['date']} {i['description']}')
+        print(f'{' '.join(i['from'])} -> {' '.join(i['to'])}')
+
+        print(f'{i['operationAmount']['amount']} {i['operationAmount']['currency']['name']}')
+        print()
+
+
+print_operation(modify_operation_dict)
